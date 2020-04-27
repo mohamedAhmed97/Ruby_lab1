b10501d99a426ce4c51015dae2c0045452d6d654
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-04-27 16:21:11 +0200

    finish lab4

---
+++ lab4/lab4/app/serializers/item_serializer.rb
@@ -0,0 +1,3 @@
+class ItemSerializer < ActiveModel::Serializer
+  attributes :item_name, :item_description
+end
---
+++ lab4/lab4/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :items
+  resources :posts
+  resources :users, param: :_username
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
